9c8aff98877df725f55f334e67379fa1e5a351a1
Jesslynwong <jesslynwjx@gmail.com>
2022-10-12 21:35:15 +0800

    feat WIP: brush

--- demo/index.ts
+++ demo/index.ts
@@ -1,4 +1,41 @@
 import './index.css'
 
-var message:string = "Hello world" 
-console.log(message)
+const canvas = document.getElementById('main_canvas') as HTMLCanvasElement | null
+let context = canvas.getContext('2d')
+
+// 配置
+let is_drawing = false
+let draw_color = 'black'
+let draw_width = 20
+
+// 事件
+canvas.addEventListener('mousedown', start, false)
+canvas.addEventListener('mousemove', draw, false)
+canvas.addEventListener('mouseup', stop, false)
+
+
+// 方法
+function start(e:any):void {
+    is_drawing = true
+    context.beginPath()    
+    context.strokeStyle = draw_color
+    context.lineCap = 'round'
+    context.lineJoin = 'round'
+    context.lineWidth = draw_width
+    context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+}
+
+function draw(e:any):void {
+    if (!is_drawing) return
+    
+    context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+    
+    context.stroke()
+    
+}
+
+function stop(e:any):void {
+    if (is_drawing) {
+        is_drawing = false
+    }
+}
---
+++ js/demo/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./index.css");
+var message = "Hello world";
+console.log(message);
